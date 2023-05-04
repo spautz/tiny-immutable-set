9930dea22b7362f06ea90c56df6b4db53bbd3875
Steven Pautz <spautz@gmail.com>
2023-05-03 21:35:16 -0400

    Add Github workflows for preparing and publishing releases (#17)
    * Add Github Actions for preparing and publishing releases
    
    * Cleanup
    
    * Update changeset

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,13 +2,14 @@ import { configDefaults, defineConfig } from 'vitest/config';
 
 export default defineConfig({
   test: {
-    environment: 'jsdom',
+    // environment: 'jsdom',
     globals: true,
 
     // This gets resolved *per project* (each package, plus the root)
     setupFiles: './setupTests.ts',
 
     coverage: {
+      provider: 'c8',
       exclude: [...configDefaults.exclude, '**/__tests__/**', '**/legacy-types/**'],
       reporter: ['html', 'lcov'],
     },
