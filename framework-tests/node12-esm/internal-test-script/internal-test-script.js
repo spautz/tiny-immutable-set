46dc75bdc7c0cc8883f7526c5c88323c5151acd0
Steven Pautz <spautz@gmail.com>
2024-02-21 21:43:08 -0500

    Add extra cjs/esm cases for specific Node versions

---
+++ framework-tests/node12-esm/internal-test-script/internal-test-script.js
@@ -0,0 +1,24 @@
+#!/usr/bin/env node
+
+import { set } from 'tiny-immutable-set';
+
+const originalValue = {
+  someNumbers: {
+    one: 1,
+    two: 1,
+  },
+};
+
+const newValue = set(originalValue, 'someNumbers.two', 2);
+
+if (newValue === originalValue) {
+  throw new Error('tiny-immutable-set did not create a new object');
+}
+if (newValue.someNumbers === originalValue.someNumbers) {
+  throw new Error('tiny-immutable-set did not create a new object within the object');
+}
+if (newValue.someNumbers.two !== 2) {
+  throw new Error('tiny-immutable-set did not set the value properly');
+}
+
+console.log('newValue: ', newValue);
